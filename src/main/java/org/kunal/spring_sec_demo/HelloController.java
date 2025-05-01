fb21323b5728792acc645cb7a37c6eec1a9f70d0
Kunal Saha <kunalsaha1107@gmail.com>
2025-05-01 19:36:29 +0530

    feat: Securing

--- src/main/java/org/kunal/spring_sec_demo/HelloController.java
+++ src/main/java/org/kunal/spring_sec_demo/HelloController.java
@@ -1,5 +1,6 @@
 package org.kunal.spring_sec_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet() {
-        return "Hello World";
+    public String greet(HttpServletRequest httpServletRequest) {
+        return "Hello World " + httpServletRequest.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest httpServletRequest) {
+        return "<h1>Kunal</h1> "+httpServletRequest.getSession().getId();
     }
 
 }
