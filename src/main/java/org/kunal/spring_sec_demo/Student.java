c5bd8f5a445d0593b3ab315854f6de7ebb68e6ca
Kunal Saha <kunalsaha1107@gmail.com>
2025-05-02 16:57:25 +0530

    feat: SecurityConfig

---
+++ src/main/java/org/kunal/spring_sec_demo/Student.java
@@ -0,0 +1,14 @@
+package org.kunal.spring_sec_demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String techStack;
+}
