0bd95957e86a69c4594fd17af0fa3e49fc03922a
Kunal Saha <kunalsaha1107@gmail.com>
2025-05-02 17:18:05 +0530

    feat: MultipleUsers

--- src/main/java/org/kunal/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/org/kunal/spring_sec_demo/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.http.UserDetailsServiceFactoryBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +26,25 @@ public class SecurityConfig {
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder() //don't use in production
+                                    .username("Kunal")
+                                    .password("Chordata")
+                                    .roles("USER")
+                                    .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder() //don't use in production
+                .username("Admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
