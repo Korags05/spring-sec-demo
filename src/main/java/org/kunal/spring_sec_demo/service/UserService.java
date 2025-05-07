b22d01ca99220320083cc783543d3f29efe6f451
Kunal Saha <kunalsaha1107@gmail.com>
2025-05-07 12:01:06 +0530

    feat: BCrypt

--- src/main/java/org/kunal/spring_sec_demo/service/UserService.java
+++ src/main/java/org/kunal/spring_sec_demo/service/UserService.java
@@ -3,6 +3,8 @@ package org.kunal.spring_sec_demo.service;
 import org.kunal.spring_sec_demo.dao.UserRepo;
 import org.kunal.spring_sec_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +12,10 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
 
     public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
